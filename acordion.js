86decf50a2bc2d1eebe99b0fadf3fb9deb6290b2
XD <61342065+renegc@users.noreply.github.com>
2023-08-25 00:49:00 -0600

    la primera

---
+++ acordion.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var acc = document.getElementsByClassName("accordion-header");
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.nextElementSibling.classList.toggle("show");
+        });
+    }
+});
